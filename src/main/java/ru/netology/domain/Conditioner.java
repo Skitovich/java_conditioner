0299d30278ee9b21218c48120e8683b63215b269
Skitovich Ruslan <zloycvetok@gmail.com>
2020-09-24 17:38:18 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -0,0 +1,73 @@
+package ru.netology.domain;
+
+public class Conditioner {
+
+    private String name;
+    private int maxTemperature;
+    private int minTemperature;
+    private int currentTemperature;
+    private boolean on;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxTemperature() {
+        maxTemperature = 15;
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(int maxTemperature) {
+
+        this.maxTemperature = maxTemperature;
+    }
+
+    public int getMinTemperature() {
+        minTemperature = -15;
+        return minTemperature;
+    }
+
+    public void setMinTemperature(int minTemperature) {
+
+        this.minTemperature = minTemperature;
+    }
+
+
+    public int getCurrentTemperature() {
+
+        return currentTemperature;
+    }
+
+    public void setCurrentTemperature(int currentTemperature) {
+        if (currentTemperature > maxTemperature) {
+            return;
+        }
+        if (currentTemperature < minTemperature) {
+            return;
+        }
+        this.currentTemperature = currentTemperature;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+
+    public void increaseCurrentTemperature() {
+      int increaseTemperature = getCurrentTemperature() + 1;
+      setCurrentTemperature(increaseTemperature);
+    }
+    public void decreaseCurrentTemperature() {
+        int decreaseTemperature = getCurrentTemperature() - 1;
+        setCurrentTemperature(decreaseTemperature);
+    }
+}
---
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -0,0 +1,36 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+
+    @Test
+    public void increaseCurrentTemperature() {
+      Conditioner conditioner = new Conditioner();
+        int maxTempMinusOne = conditioner.getMaxTemperature() - 1;
+
+        conditioner.setCurrentTemperature(maxTempMinusOne);
+        conditioner.increaseCurrentTemperature();
+        conditioner.increaseCurrentTemperature();
+        assertEquals(conditioner.getMaxTemperature(),conditioner.getCurrentTemperature());
+
+
+    }
+
+    @Test
+    public void decreaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+            int minTempPlusOne = conditioner.getMinTemperature() + 1;
+
+            conditioner.setCurrentTemperature(minTempPlusOne);
+
+            conditioner.decreaseCurrentTemperature();
+            conditioner.decreaseCurrentTemperature();
+            assertEquals(conditioner.getMinTemperature(),conditioner.getCurrentTemperature());
+    }
+
+
+
+}
